d04f53a55d7ab0bd53c3ccbd68a072c6b5d2b5bc
Ayberk Tunca <140557959+Kepitition@users.noreply.github.com>
2023-12-08 11:54:43 +0300

    Blind 75 -- 3/75 Longest Palindromic Substring
    https://leetcode.com/problems/longest-palindromic-substring/submissions/?envType=list&envId=xi4ci4ig

---
+++ Longest_palindrome.py
@@ -0,0 +1,24 @@
+#BRUTE FORCE APPROACH
+'''
+def is_palindrome(s):
+    s = s.lower()
+    s = ''.join(char for char in s if char.isalnum())
+    return s == s[::-1]
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        long_polindrome = ''
+        if is_palindrome(s) == True:
+            return s
+        for i in range(len(s)):
+            for k in range(i,len(s)):
+                deger = s[i:k+1]
+                if is_palindrome(s[i:k+1]):
+                    if len(s[i:k+1]) > len(long_polindrome):
+                        long_polindrome = s[i:k+1]
+        return long_polindrome
+    
+s = "ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy"
+a = Solution()
+print(a.longestPalindrome(s))
+'''
